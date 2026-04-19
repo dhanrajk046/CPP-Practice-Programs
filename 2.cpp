48c5662f0a0a90a1da485b03cbeff0ddca510782
dhanrajk046 <dhanrajk046@gmail.com>
2026-04-20 00:49:38 +0530

    first commit

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+// #include<conio.h>
+using namespace std;
+
+int main()
+{
+    // clrscr();
+    int value1, value2, sum;
+    cout<<"Enter First Value:";
+    cin>>value1;
+    cout<<"Enter second value:";
+    cin>>value2;
+    sum = value1 + value2;
+    cout<<"The sum of given values is:";
+    cout<<sum;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Header for input and output
+using namespace std; // Use standard library names directly
+
+int main() {
+    cout << "Hello World!"; // Output text to the console
+    return 0;               // Signal successful program execution
+}
