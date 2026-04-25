722d2b7d72495f165d087bf2e7fe1a60e5b9846e
dhanrajk046 <dhanrajk046@gmail.com>
2026-04-25 22:38:22 +0530

    Q3. of interviewbit solved

---
+++ 13.cpp
@@ -0,0 +1,14 @@
+//Q3. of interviewbit solved
+#include <iostream>
+using namespace std;
+
+int main()  {
+    int a;
+    char ch;
+    // cout<<"ch = ";  
+    cin>>ch;
+    cout<<(int)ch;
+    
+    //Your code goes here
+    return 0;
+}
