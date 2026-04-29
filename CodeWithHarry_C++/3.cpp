17e9ba645420acf6bd4cb5df63503e141d4597bb
dhanrajk046 <dhanrajk046@gmail.com>
2026-04-30 01:04:16 +0530

    Lect-3 Variables and comments

---
+++ CodeWithHarry_C++/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//This is a simgle line comment.
+
+/*This 
+is 
+a 
+Multi-line
+Comment
+*/
+
+int main(){
+    int sum = 6;
+    cout<<"Hello World"<<sum;
+    return 0;
+}
