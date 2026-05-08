472d2b1d952635cda18e3db27cd722f83dd7f4dd
dhanrajk046 <dhanrajk046@gmail.com>
2026-05-08 23:29:52 +0530

    Add If Else Statement Practice (Lecture 4)

---
+++ DSA With C++ - Coder Army/If_else.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int package;
+    // cin>>package;
+
+    // if(package>10)
+    // {
+    //     cout<<"Accepted";
+    // }
+    // else
+    // {
+    //     cout<<"Rejected";
+    // }
+
+    // int marks;
+    // cout<<"Enter your marks: ";
+    // cin>>marks;
+
+    // if(marks>33)
+    // cout<<"pass";
+    // else
+    // cout<<"Fail";
+    // return 0;
+
+    // int a,b;
+    // cout<<"Enter your first number: ";
+    // cin>>a;
+    // cout<<"Enter your second number: ";
+    // cin>>b;
+
+    // if(a>b)
+    // {
+    //     cout<<"Yes";
+    // }
+    // else{
+    //     cout<<"No";
+    // }
+
+    // int number;
+    // cout<<"Enter your first number: ";
+    // cin>>number;
+
+    // if(number%2==0)
+    // {
+    //     cout<<"Even";
+    // }
+    // else
+    // {
+    //     cout<<"Odd";
+    // }
+
+    int number;
+    cin>>number;
+
+    if(number>0)
+    cout<<"Positive";
+    else if (number == 0)
+    cout<<"0";
+    else
+    cout<<"Negative";
+    
+}
