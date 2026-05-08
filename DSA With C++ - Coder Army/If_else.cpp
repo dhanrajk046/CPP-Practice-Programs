482427ae93ec1a66eef7e206874043abaaa43f09
dhanrajk046 <dhanrajk046@gmail.com>
2026-05-09 02:17:14 +0530

    Add If Else and For Loop (Lecture 4)

--- DSA With C++ - Coder Army/If_else.cpp
+++ DSA With C++ - Coder Army/If_else.cpp
@@ -51,14 +51,123 @@ int main(){
     //     cout<<"Odd";
     // }
 
-    int number;
-    cin>>number;
+    // int number;
+    // cin>>number;
 
-    if(number>0)
-    cout<<"Positive";
-    else if (number == 0)
-    cout<<"0";
-    else
-    cout<<"Negative";
+    // if(number>0)
+    // cout<<"Positive";
+    // else if (number == 0)
+    // cout<<"0";
+    // else
+    // cout<<"Negative";
+
+    // char c ;
+    // cin>>c;
+
+    // if(c=='a')
+    // cout<<"vowel";
+    // else if(c=='e')
+    // cout<<"vowel";
+    // else if(c=='i')
+    // cout<<"vowel";
+    // else if(c=='o')
+    // cout<<"vowel";
+    // else if(c=='u')
+    // cout<<"vowel";
+    // else 
+    // cout<<"consonant";
+
+    // int num;
+    // cin>>num;
+
+    // if(num==1)
+    // cout<<"Monday";
+    // else if(num==2)
+    // cout<<"Tuesday";
+    // else if(num==3)
+    // cout<<"Wednesday";
+    // else if(num==4)
+    // cout<<"Thursday";
+    // else if(num==5)
+    // cout<<"Friday";
+    // else if(num==6)
+    // cout<<"Saturday";
+    // else if(num==7)
+    // cout<<"Sunday";
+    // else
+    // cout<<"Enter valid day";
+
+    // cout<<"Coder Army\n";
+    // cout<<"Coder Army\n";
+    // cout<<"Coder Army\n";
+    // cout<<"Coder Army\n";
+    // cout<<"Coder Army\n";
+
+    // for (int count = 0;count<=5; count=count+1)
+    // {
+    //     /* code */
+    //     cout<<"Coder Army\n";
+    // }
+
+    // for (int i = 1; i <=10 ; i++)
+    // {
+    //     /* code */
+    //     cout<<"Hello\n";
+    // }
     
+
+    // int n;
+    // cin>>n;
+    // for (int count = 1;count<=n; count= count+1)
+    // {
+    //     /* code */
+    //     cout<<count<<endl;
+    // }
+    
+    // int n;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+
+    // for (int count = 1;count<=n; count= count+1)
+    // {
+    //     /* code */
+    //     cout<<count<<" Square is: "<<count*count<<endl;
+    // }
+
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+
+    // for (int i = 2; i <=num; i=i+2)
+    // {
+    //     /* code */
+    //     cout<<i<<endl;
+    // }
+
+    // for (int i = 0; i < num; i++)
+    // {
+    //     /* code */
+    //     if(i%2==0)
+    //     {
+    //         cout<<i<<" ";
+    //     }
+        // else
+        // {
+            
+        // }
+    // }
+    for (int i = 0; i < num; i++)
+    {
+        /* code */
+        if(i%2==0)
+        {
+            cout<<" ";
+        }
+        else{
+            cout<<i<<" ";
+        }
+    }
+    
+    
+
 }
