dad6f55a934323c011813394d49e8073e36abe2d
dhanrajk046 <dhanrajk046@gmail.com>
2026-05-11 04:00:21 +0530

    Add Pattern Printing (Lecture 6)

---
+++ DSA With C++ - Coder Army/06_Pattern.cpp
@@ -0,0 +1,173 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int row, col;
+
+    // for ( row= 1; row<=5; row=row+1)
+    // {
+    //     /* code */
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         // cout<<"* ";
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // int row, col;
+
+    // for(row=1; row<=4; row=row+1)
+    // {
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         cout<<10<<" ";
+    //         // cout<<"10 ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int row, col;
+
+    // for (row = 1; row <= 5; row=row+1)
+    // {
+    //     for(col=1;col<=5;col=col+1)
+    //     /* code */
+    //     {
+    //         cout<<row<<" ";
+    //         // cout<<"row ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int row, col;
+
+    // for ( row = 1; row <= 5; row=row+1)
+    // {
+    //     /* code */
+    //     for (col = 1; col <= 5; col=col+1)
+    //     {
+    //         /* code */
+    //         for ( col = 1; col <= 5; col=col+1)
+    //         {
+    //             /* code */
+    //             cout<<col<<" ";
+    //         }
+    //         cout<<endl;
+            
+    //     }
+        
+    // }
+
+    // int row, col;
+
+    // for (row = 1; row<=5; row=row+1)
+    // {
+    //     /* code */
+    //     for (col = 5; col >= 1; col=col-1)
+    //     {
+    //         /* code */
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // int row, col;
+
+    // for (row = 1; row<=5; row=row+1)
+    // {
+    //     /* code */
+    //     for (col = 1; col <= 5; col=col+1)
+    //     {
+    //         /* code */
+    //         cout<<col*col<<" ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // int row, col;
+
+    // for (row = 1; row<=5; row=row+1)
+    // {
+    //     /* code */
+    //     for (col = 1; col <= 5; col=col+1)
+
+    //     {
+    //         /* code */
+    //         cout<<col*col*col<<" ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // int row, col;
+
+    // for ( row = 1; row <= 5; row=row+1)
+    // {
+    //     /* code */
+    //     char name ='a'+row-1;
+    //     for (col= 1; col <=5; col=col+1)
+    //     {
+    //         /* code */
+    //         cout<<name<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    //  int row;
+    //  char col;
+
+    // for ( row = 1; row <= 5; row=row+1)
+    // {
+    //     /* code */
+    //     for (col= 'a'; col <='e'; col=col+1)
+    //     {
+    //         /* code */
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int row, col;
+
+    // for ( row = 1; row <= 5; row=row+1)
+    // {
+    //     /* code */
+    //     // char name ='a'+row-1;
+    //     for (col= 1; col <=5; col=col+1)
+    //     {
+    //         /* code */
+    //         char name = 'a' + col-1;
+    //         cout<<name<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int row, col;
+
+    // int count = 1;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         cout<<count<<" ";
+    //         count =count+1;
+    //     }cout<<endl; 
+    // }
+
+    int row, col;
+
+    int count = 1;
+    for(row=1;row<=5;row=row+1)
+    {
+        for(col=1;col<=5;col=col+1)
+        {
+            cout<<(row-1)*5+col<<" ";
+        }cout<<endl; 
+    }
+    
+    
+}
