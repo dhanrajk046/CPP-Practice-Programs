1ba1593980586d4ff4aa7c3346edd7d421d00a7f
dhanrajk046 <dhanrajk046@gmail.com>
2026-05-11 23:26:55 +0530

    Add Arrays & Pointer Arthimetic in C++ (Lecture 13)

---
+++ CodeWithHarry_C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+cout<<"The value of *p is"<<*p<<endl;
+    cout<<"The value of *(p+1) is"<<*p<<endl;
+    cout<<"The value of *(p+2) is"<<*p<<endl;
+    cout<<"The value of *(p+3) is"<<*p<<endl;
+    
