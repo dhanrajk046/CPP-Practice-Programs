e398c239f31eff53528d90d8ca44dd9e2db8f2a3
dhanrajk046 <dhanrajk046@gmail.com>
2026-05-15 20:37:20 +0530

    Add Patterns part-3 (Lecture -3)

---
+++ DSA With C++ - Coder Army/08_Pattern-3.cpp
@@ -0,0 +1,186 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int row,col;
+    // int n;
+    
+    // cout<<"Input the number: ";
+    // cin>>n;
+
+    // for(row=1;row<=n;row=row+1)
+    // {
+    //     for(col=1;col<=n-row;col=col+1)
+    //     cout<<"  ";
+
+    //     for(col=1;col<=row;col=col+1)
+    //     cout<<"* ";
+
+    //     cout<<endl;
+    // }
+    
+
+    // int row,col;
+    // int n;
+    
+    // cout<<"Input the number: ";
+    // cin>>n;
+
+    // for(row=1;row<=n;row=row+1)
+    // {
+    //     for(col=1;col<=n-row;col=col+1)
+    //     cout<<"  ";
+
+    //     for(col=1;col<=row;col=col+1)
+    //     cout<<row<<" ";
+
+    //     cout<<endl;
+    // }
+
+    // int row,col;
+    // int n;
+    // cout<<"Enter the input: ";
+    // cin>>n;
+
+    // for ( row = 1; row <= n; row=row+1)
+    // {
+    //     /* space print */
+    //     for (col = 1; col <= n-row; col=col+1)
+    //     cout<<"  ";
+
+    //     //Number print
+    //     for (col = 1; col <= row; col=col+1)
+    //     cout<<col<<" ";
+
+    //     cout<<endl;
+        
+        
+    // }
+
+    // int row,col;
+    // int n;
+
+    // cout<<"Enter the input: ";
+    // cin>>n;
+
+    // for ( row= 1; row <=n; row=row+1)
+    // {
+    //     /* Space print */
+    //     for ( col = 1; col <=n-row;col=col+1)
+    //     cout<<"  ";
+
+    //     /* Char print*/
+    //     for (col = 1; col<=row; col=col+1)
+    //     {
+
+    //     char name = 'A'+col-1;
+    //     cout<<name<<" ";
+    //     }
+
+    //     cout<<endl;
+        
+        
+    // }
+
+    // int row, col;
+    // int n;
+
+    // cout<<"Enter the input: ";
+    // cin>>n;
+
+    // for(row=1;row<=n;row=row+1)
+    // {
+    //     for(col=1;col<=n-row;col=col+1)
+    //     {
+    //         cout<<"  ";
+    //     }
+        
+    //     for(col=1;col<=row;col=col+1)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+
+    //H.W.
+
+    //First Pattern
+
+    // int row=5, col=6;
+    // int n;
+
+    // cout<<"Enter the input: ";
+    // cin>>n;
+
+    // for (row = 1; row <=5 ; row=row+1)
+    // {
+    //     /* Print number */
+    //     for(col=1;col<=6;col=col+1)
+    //     cout<<n<<" ";
+    
+    //     cout<<endl;
+    // }
+
+
+    //Second Pattern
+
+    // int row=6,col=5;
+    // int n;
+
+    // cout<<"Enter the input: ";
+    // cin>>n;
+
+    // for(row = 1; row <=6; row=row+1)
+    // {
+    //     /*Print number*/
+    //     for (col = 1;col <= 5; col=col+1)
+    //     cout<<col*col<<" ";
+
+    //     cout<<endl;
+        
+    // }
+
+    //Third Pattern
+
+    // int col, row;
+    // int n;
+
+    // cout<<"Enter the input: ";
+    // cin>>n;
+
+    // for(row=1; row<=5; row=row+1)
+    // {
+    //     for ( col = 1; col <= 6; col= col+1)
+    //     {
+    //         /* code */
+    //         cout<<col*col*col<<" ";
+
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    //Fourth Pattern
+
+    int col, row;
+    int n;
+
+    cout<<"Enter the input: ";
+    cin>>n;
+
+    for ( row = 1; row <=5; row=row+1)
+    {
+        /* code */
+        for ( char name ='F'; name<='K'; name=name+1)
+        {
+            /* code */
+            cout<<name<<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+
+
+}
