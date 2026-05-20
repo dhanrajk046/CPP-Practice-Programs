8d298a4400f971fbfc476605938053abd94e5bdd
dhanrajk046 <dhanrajk046@gmail.com>
2026-05-20 15:55:12 +0530

    Add Operators in C++ (Lecture 10)

---
+++ DSA With C++ - Coder Army/10_Operators_in_C++.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<13.8/2;
+
+    // int a=10;
+    // int b=++a;
+
+    // int a=10;
+    // int b=a++;
+
+    // int a=10;
+    // int b=a--;
+
+    // int a=10;
+    // int b=--a;
+    // cout<<b<<" "<<a;
+    
+    // return 0;
+
+    // comparison operator
+
+    // 1 or 0
+
+    // int a,b;
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+    // cout<<"Enter the value of b: ";
+    // cin>>b;
+
+    // // if(a==b)
+    // // cout<<"Yes";
+    // // else
+    // // cout<<"No";
+
+    // // >, <, >=, <=, !=
+
+    // if(a!=b)
+    // cout<<"Yes";
+    // else
+    // cout<<"No";
+    
+    // Logical Operator
+
+    // int a,b,c;
+    // cin>>a>>b>>c;
+
+    // if (a>b && a>c)
+    // cout<<"Yes";
+    // else
+    // cout<<"No";
+    
+
+    //or
+
+    // char name = 'b';
+
+    // if(name=='a'||name=='e'||name=='i'||name=='o'||name=='u')
+    // cout<<"Vowel";
+    // else
+    // cout<<"consonant";
+
+    // cout<<!28<<endl;
+
+    // bitwise Operator
+
+    //& Operator, |, ^, <<, >>, ~
+    // int ans= ~5;
+    // cout<<"My answer is : "<<ans<<endl;
+
+    //Assignment operator
+
+    // int a = 10;
+    // a+=15; //a=a+15
+    // cout<<a;
+
+    if(8>5>6>9*4)
+    cout<<"Yes";
+    else
+    cout<<"No";
+
+}
