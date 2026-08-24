634186c8403c131f87eee95ff031173e2a123770
dhanrajk046 <dhanrajk046@gmail.com>
2026-08-24 18:35:34 +0530

    Lecture 9 Homework Complete - DSA CoderArmy

--- DSA_With_C++-CoderArmy/08_Pattern-3.cpp
+++ DSA_With_C++-CoderArmy/08_Pattern-3.cpp
@@ -201,32 +201,229 @@
 
 // Q4. Fourth Pattern
 
- #include<iostream>
+//  #include<iostream>
+// using namespace std;
+
+// int main() 
+// {
+//     int n;
+//     cin>>n;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         /* code */
+//         for (int j = 1; j <= n-i; j++)
+//         {
+//             /* code */
+//             cout<<"  ";
+//         }
+
+//         // Characters
+//         for (int j = 0; j < i; j++)
+//         {
+//             /* code */
+//             cout<< char('A' + n - 1 - j) << " ";
+//         }
+//         cout<< endl;
+//     }
+//     return 0;
+// }
+
+
+// Homework
+
+// Q1. First Pattern
+
+// #include<iostream>
+// using namespace std;
+
+// int main() 
+// {
+//     int n;
+//     cin>>n;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         /* Space before stars*/
+//         for (int j = 1; j <=n-i; j++)
+//         {
+//             /* code */
+//             cout<<" ";
+//         }
+
+//         // Stars
+//         for (int j = 1; j <= i; j++)
+//         {
+//             /* code */
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// Q2.Second Pattern
+
+// #include<iostream>
+// using namespace std;
+
+// int main() 
+// {
+//     int n;
+//     cin>>n;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         /* code */
+//         for (int j = 1; j <= n-i; j++)
+//         {
+//             /* code */
+//             cout<<"  ";
+//         }
+
+//         // Numbers
+//         for (int  j = 1; j <= 2*i-1; j++)
+//         {
+//             /* code */
+//             cout<<j<<" ";    
+//         } 
+//         cout<<endl;
+//     }
+//     return 0; 
+// }
+
+
+
+// Q3. Third Pattern
+// #include<iostream>
+// using namespace std;
+
+// int main() 
+// {
+//     int n;
+//     cin>>n;
+
+//     for (int i = 1; i <=  n; i++)
+//     {
+//         /* Spaces */
+//         for (int j = 1; j < n-i; j++)
+//         {
+//             /* code */
+//             cout<<"  ";
+//         }
+
+//         // Increasing characters
+//         for (int j = 1; j <= i; j++)
+//         {
+//             /* code */
+//             cout<< char('A'+ j - 1)<<" ";
+//         }
+
+//         // Decreasing characters
+//         for (int j = i-1; j >=1; j--)
+//         {
+//             /* code */
+//             cout<< char('A'+ j - 1)<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+        
+        
+// Q4. Fourth Pattern
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+
+//     // Upper half
+//     for (int i = 1; i <= n; i++)
+//     {
+//         //Spaces
+//         for (int j = 1; j <= n-i; j++)
+//         {
+//             /* code */
+//             cout<<"  ";
+//         }
+
+//         // Stars
+//         for (int j = 1; j <= i; j++)
+//         {
+//             /* code */
+//             cout<<"* ";
+
+//         }
+//         cout<<endl;  
+//     }
+
+//     // Lower half
+//     for (int i = n-1; i >= 1; i--)
+//     {
+//         /* Spaces */
+//         for (int j = 1; j <= n-i; j++)
+//         {
+//             cout<<"  ";
+//         }
+
+//         // Stars
+//         for (int j = 1; j <= i; j++)
+//         {
+//             /* code */
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+#include<iostream>
 using namespace std;
 
-int main() 
+int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
 
+    // Upper half
     for (int i = 1; i <= n; i++)
     {
-        /* code */
-        for (int j = 1; j <= n-i; j++)
+        // Spaces: Changed from "  " to " "
+        for (int j = 1; j <= n - i; j++)
         {
-            /* code */
-            cout<<"  ";
+            cout << " ";
         }
 
-        // Characters
-        for (int j = 0; j < i; j++)
+        // Stars
+        for (int j = 1; j <= i; j++)
         {
-            /* code */
-            cout<< char('A' + n - 1 - j) << " ";
+            cout << "* ";
         }
-        cout<< endl;
+        cout << endl;  
     }
+
+    // Lower half
+    // Changed starting point from n-1 to n to duplicate the middle row
+    for (int i = n; i >= 1; i--)
+    {
+        // Spaces: Changed from "  " to " "
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+
+        // Stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
-
-
