60c45e908b0d205030e963aab9b55878c83062e2
dhanrajk046 <dhanrajk046@gmail.com>
2026-08-28 20:44:38 +0530

    Fixed Bugs - Spelling mistake of Sum

--- DSA_With_C++-CoderArmy/02_Second.cpp
+++ DSA_With_C++-CoderArmy/02_Second.cpp
@@ -11,7 +11,7 @@ int main()
     // cout<<"Enter your second number: ";
     // cin>>b;
 
-    // cout<<"Sume of numbers is: "<<a+b;
+    // cout<<"Sum of numbers is: "<<a+b;
 
     int a =1234;
     char c = 'b';
